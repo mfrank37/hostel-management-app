886fcd18f35c24ace7255ea80820c1ccda7ad48c
Mungeri Frank <mungerifrank@gmail.com>
2021-02-01 13:43:16 +0200

    react to changes and update states

--- src/controllers/handleActions.js
+++ src/controllers/handleActions.js
@@ -1,5 +1,3 @@
-import { rooms } from "../data";
-
 export default function handleActions(state, action) {
   let stud;
   switch (action.type) {
@@ -22,20 +20,42 @@ export default function handleActions(state, action) {
         students: state.students,
         rooms: state.rooms
       }
-      case 'UNPLACE_STUDENT':
-        // remove him from the room
+    case 'UNPLACE_STUDENT':
+      // remove him from the room
+      if(typeof action.student === 'string') {
+        state.students.find((std) => std.name === action.student).room = '';
+        stud = state.students.find((std) => std.name === action.student);
+      } else {
         state.students.find((std) => std.id === action.student).room = '';
         stud = state.students.find((std) => std.id === action.student);
-        const list = new Set(rooms.find((room) => room.students.includes(stud.name)).students);
-        list.delete(stud.name);
-        console.log(list);
-        rooms.find((room) => room.students.includes(stud.name)).students = Array.from(list);
-        console.log(stud);
-        return {
-          students: state.students,
-          rooms: state.rooms
-        }
-        default:
-          return state;
+      }
+      const list = new Set(state.rooms.find((room) => room.students.includes(stud.name)).students);
+      list.delete(stud.name);
+      console.log(list);
+      state.rooms.find((room) => room.students.includes(stud.name)).students = Array.from(list);
+      console.log(stud);
+      return {
+        students: state.students,
+        rooms: state.rooms
+      }
+    case 'ADD_STUDENT_TO_ROOM':
+      try {
+        stud = state.students.find((std) => std.name === action.student);
+        const removeFrom = new Set(state.rooms.find((room) => room.students.includes(stud.name)).students);
+        console.log(removeFrom);
+        removeFrom.delete(stud.name);
+        state.rooms.find((room) => room.students.includes(stud.name)).students = Array.from(removeFrom);
+      } catch (e) {
+
+      }
+      stud.room = action.room;
+      state.rooms.find(room=> room.name === action.room).students.push(stud.name);
+      state.students = state.students.map(STD=> STD.id === stud.id ? stud : STD);
+      return {
+        students: state.students,
+        rooms: state.rooms
+      }
+    default:
+      return state;
   }
 };
--- src/routes/room.jsx
+++ src/routes/room.jsx
@@ -1,14 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Header from '../components/header';
 import Rooms from './rooms';
 
 
-const Room = () => {
+const Room = (props) => {
 
   // const { params } = this.props.match;
   const { name } = useParams();
-  const { students, rooms } = useSelector((state) => ({students: state.students, rooms: state.rooms}));
+  const { students } = useSelector((state) => ({students: state.students}));
+  const { rooms } = useSelector((state) => ({rooms: state.rooms}));
+  
   const index =  rooms.findIndex((room) => room.name === name);
   if(index < 0) return <Rooms notFound={true}/>;
 
@@ -17,7 +19,6 @@ const Room = () => {
     : rooms[index].beds < rooms[index].students.length 
     ? <p className='exceed'>STATUS : exceed</p>
     : <p className='meet'>STATUS : meet expected</p>;
-  console.log(STATUS);
   const newStudents = () => {
     const set = new Set();
     students.forEach(std => set.add(std.name));
@@ -30,6 +31,23 @@ const Room = () => {
     rooms[index].students.forEach((stud) => set.delete(stud));
     return Array.from(set);
   };
+  
+  const dispatch = useDispatch();
+  const placeStudentHere = ({target}) => {
+    dispatch({
+      type: 'ADD_STUDENT_TO_ROOM',
+      room: name,
+      student: target.innerHTML
+    });
+  }
+  const removeStudentsFromHere = ({target}) => {
+    dispatch({
+      type: 'UNPLACE_STUDENT',
+      student: target.innerHTML
+    });
+  }
+
+  let key = 1;
 
   return (
     <>
@@ -39,19 +57,19 @@ const Room = () => {
           <p>BEDS AVAILABLE : { rooms[index].beds }</p>
           <p>STUDENTS PLACED HERE : { rooms[index].students.length }</p>
           <ul className='students-list'>
-            { rooms[index].students.map(stud => <li>{stud}</li>)}
+            { rooms[index].students.map(stud => <li className='currently-here' onClick={removeStudentsFromHere}>{stud}</li>)}
           </ul>
           <p style={{textAlign: 'center', backgroundColor: '#ccc'}}>PLACE NEW STUDENTS HERE</p>
         <div className='placing-list'>
-          <li className='move left'></li>
-            { newStudents().map(student => <li className='stud'>{student}</li>) }
-          <li className='move right'></li>
+          <li key='left1' className='move left'></li>
+            { newStudents().map(student => <li key={++key} className='stud' onClick={placeStudentHere}>{student}</li>) }
+          <li key='right1' className='move right'></li>
         </div>
         <p style={{textAlign: 'center', backgroundColor: '#ccc'}}>REPLACE/MOVE STUDENTS HERE</p>
         <div className='moving-list'>
-          <li className='move left'></li>
-            { placedStudents().map(student => <li className='stud'>{student}</li>) }<li className='move right'>
-          </li>
+          <li key='left2' className='move left'></li>
+            { placedStudents().map(student => <li key={++key} className='stud' onClick={placeStudentHere}>{student}</li>) }
+          <li key='right2' className='move right'></li>
         </div>
       </div>
     </>
